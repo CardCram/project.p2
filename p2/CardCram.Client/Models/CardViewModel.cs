6c79c7c0efa8e9c0e4b5561ab9660b154c8a909e
Michael LaBarbiera <mlaba49@gmail.com>
2021-01-25 18:06:51 -0500

    Added view models

---
+++ p2/CardCram.Client/Models/CardViewModel.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.Configuration;
+
+namespace CardCram.Client.Models
+{
+  public class CardViewModel
+  {
+    public string Question { get; set; }
+    public string Answer { get; set; }
+    
+  }
+}
---
+++ p2/CardCram.Client/Models/DeckViewModel.cs
@@ -0,0 +1,10 @@
+using Microsoft.Extensions.Configuration;
+
+namespace CardCram.Client.Models
+{
+  public class CardViewModel
+  {
+    public List<CardViewModel> Cards { get; set; }
+    
+  }
+}
