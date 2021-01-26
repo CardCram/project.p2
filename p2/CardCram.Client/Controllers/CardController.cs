2532b551b17ad33c015c53ede84ee8af3b326301
amulyakakumanu1997 <55294384+amulyakakumanu1997@users.noreply.github.com>
2021-01-26 16:56:55 -0600

    Add files via upload

--- p2/CardCram.Client/Controllers/CardController.cs
+++ p2/CardCram.Client/Controllers/CardController.cs
@@ -1,27 +1,46 @@
-
+using System;
+using CardCram.Client.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CardCram.Client.Controllers
 {
     public class CardController : Controller
     {
+        CardViewModel NewCard = new CardViewModel();
+        DeckViewModel CardList = new DeckViewModel();
         [Route("[controller]/[action]")]
 
         [HttpPost]
         public IActionResult CreateDeck()
         {
+
+            
             return View ();
         }
 
         [HttpPost]
         public IActionResult AddCard()
         {
+            String Answer = NewCard.Answer;
+            String Question = NewCard.Question;
+            if(CardList is null)
+            {
+                CardList.Cards.Add(NewCard);
+                ViewData["CardList"] = CardList;
+            }
+
+            else
+            {
+                ViewData["CardList"] = CardList;
+            }
+            
             return View();
         }
 
         [HttpPost]
         public IActionResult DeleteCard()
         {
+            CardList.Cards.Remove(NewCard);
             return View();
         }
 
@@ -35,6 +54,13 @@ namespace CardCram.Client.Controllers
         public IActionResult DownloadDeck()
         {
             return View();
+
+        
         }
+
+
+        
+        
+            
     }
 }
