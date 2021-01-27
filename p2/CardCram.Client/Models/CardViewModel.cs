5dd84ac76a6757f7018a9d39843c23431e84ff1f
Monica <monica.brown@revature.com>
2021-01-26 23:28:16 -0500

    views added

--- p2/CardCram.Client/Models/CardViewModel.cs
+++ p2/CardCram.Client/Models/CardViewModel.cs
@@ -3,7 +3,10 @@ namespace CardCram.Client.Models
 {
     public class CardViewModel
   {
+    public int CardId { get; set; }
+    public int DeckId { get; set; }
     public string Question { get; set; }
-    public string Answer { get; set; }   
+    public string Answer { get; set; }
+    public DeckViewModel Deck {get; set; }   
   }
 }
--- p2/CardCram.Client/Models/DeckViewModel.cs
+++ p2/CardCram.Client/Models/DeckViewModel.cs
@@ -6,6 +6,8 @@ namespace CardCram.Client.Models
     public class DeckViewModel
     {
       public List<CardViewModel> Cards { get; set; }
+      public int DeckId { get; set; }
+      public string Title { get; set; }
 
     }
 }
