286fc6e4e2a3267006bee78f417f562a003ed0bf
Monica <monica.brown@revature.com>
2021-01-31 14:01:15 -0500

    installed Newtonsoft

---
+++ aspnet_api_repo/RepoAPI.Service/Controllers/CardCramController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RepoAPI.Service.Models;
+using Newtonsoft.Json;
+
+
+namespace RepoAPI.Service.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class CardCramController : ControllerBase
+  {
+    private string apiUrl = "https://api.Cram.com/v2";
+    private HttpClient _http = new HttpClient();
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+      var response = await _http.GetAsync(apiUrl);
+
+      if (response.IsSuccessStatusCode)
+      {
+        //var content = JsonConvert<CardCramModel>(await response.Content.ReadAsStringAsync());
+
+        return Ok(await response.Content.ReadAsStringAsync());
+
+      }
+
+      return NotFound();
+    }
+  }
+}
---
+++ aspnet_api_repo/RepoAPI.Service/Models/CardCramModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace RepoAPI.Service.Models
+{
+    public class CardCramModel
+    {
+        public List<string> Card { get; set; }
+        public List<string> Deck { get; set; }
+    }
+}
