3240598a9285ea599376e37c940349be0a377442
Michael LaBarbiera <mlaba49@gmail.com>
2021-01-31 12:58:42 -0500

    Uploaded domain

---
+++ p2/CardCram.Domain/Models/Card.cs
@@ -0,0 +1,9 @@
+namespace CardCram.Domain.Models
+{
+  public class Card
+  {
+    public int CardId { get; set; }
+    public string Question { get; set; }
+    public string Answer { get; set; }
+  }
+}
