fc885ea2b70351081fa3b2cee367c77604e143d3
amulyakakumanu1997 <55294384+amulyakakumanu1997@users.noreply.github.com>
2021-02-03 00:19:37 -0600

    updated MVC

--- aspnet_mvc/CardCram.Client/Models/DeckViewModel.cs
+++ aspnet_mvc/CardCram.Client/Models/DeckViewModel.cs
@@ -1,18 +1,16 @@
 using System.Collections.Generic;
-using CardCram.Domain.Models;
 using Microsoft.Extensions.Configuration;
 
 namespace CardCram.Client.Models
 {
-    public class DeckViewModel
-    {
-      public List<Card> Cards { get; set; }
-      public Card NewCard { get; set; }
-      public int DeckId { get; set; }
-      public string Title { get; set; }
-
-      public DeckViewModel() {
-        Cards = new List<Card>();
+  public class DeckViewModel
+  {
+    public List<CardViewModel> Cards { get; set; }
+    public CardViewModel NewCard { get; set; }
+    public int DeckId { get; set; }
+    public string Title { get; set; }
+    public DeckViewModel() {
+      Cards = new List<CardViewModel>();
       }
     }
 }
